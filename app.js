501d0adbf4081a17336aee646df2e91ea6415a18
Nathan Daniel Peterson <natperson@gmail.com>
2017-10-16 16:24:07 -0700

    add routes, middleware, error handlers

--- app.js
+++ app.js
@@ -1,22 +1,61 @@
-const express = require('express');
+const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const nodemon = require('nodemon')
 const port = process.env.PORT || 3000
 const errorMsg = `Error! something went wrong`
 
-app.get('/', (req, res) => {
-  res.send('Hi')
-  res.json({greet: hi})
+app.disable('x-powered-by')
+app.use(morgan('dev'))
+
+const party = [
+  {id: 1, name: "Mario"},
+  {id: 2, name: "Luigi"},
+  {id: 3, name: "Yoshi"}
+]
+
+app.get('/party', (req, res, next) => {
+  res.json(party)
 })
 
-app.use(reg, res, next) => {
-  res.status(404)
-  res.type('txt').send(errorMsg)
-}
+app.get('/party/:id', (req, res, next) => {
+  console.log(req.params.id)
+  let request = Number(req.params.id)
+  const result = party.find(partier => partier.id === request)
+  res.json({result})
+})
+
+app.get('/ping', (req, res) => {
+  throw Error('!!!!')
+  res.json({message: 'pong'})
+})
+
+app.use('/hello', (req, res, next) => {
+  console.log(`I am middleware!`)
+  next()
+})
+
+app.get('/hello/friend', (req, res) => {
+  res.json({message: `I am your 'friend'`})
+})
+
+app.get('/hello/:name', (req, res) => {
+  let name = req.params.name
+  console.log(req.params.name)
+  res.json({message: `----Hello-----${name}-----!`})
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: {message: 'Something went wrong!'}})
+})
+
+app.use((err, req, res, next) => {
+  console.log(err)
+})
 
 const listener = () => {
-  (`listening on ${port}`)
+  console.log(`listening on ${port}`)
 }
 
 app.listen(port, listener)
